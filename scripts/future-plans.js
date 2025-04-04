b79f2a849603ca88e45aba8cdcad880b75d0d6de
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-04-04 17:43:02 +0200

    Update future-plans.js FIX selectedLanguage rev00

--- scripts/future-plans.js
+++ scripts/future-plans.js
@@ -1,5 +1,7 @@
 "use strict"
 
+selectedLanguage = localStorage.getItem("languageVariable");
+
 // Toggle info-div (week schedule)
 const toggleLinkWeek = document.getElementById("toggle-link-week");
 const contentWeek = document.getElementById("content-week");
