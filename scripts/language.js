835bf52b6d05c8151c09a65150686d3d51e95738
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-07-20 14:56:19 +0200

    Update language.js FIX (add info text)

--- scripts/language.js
+++ scripts/language.js
@@ -242,6 +242,10 @@ const futureDesc = {
   sv: `Hantera <u id="current-plan-words">Framtidsplaner</u> och visa <u id="past-tasks-words">Tidigare händelser</u>:
     <ul>
       <li id="add-list">Lägg till uppgift / Ladda upp fil (Schema).</li>
+        <ul>
+          <li>Tider sorteras (Format: tt:mm)</li>
+          <li>Återkommande uppgifter kan läggas till (veckodag, veckor, månad, år + antal dagars varaktighet)</li>
+        </ul>
       <li id="save-list">Spara fil (Schema).</li>
       <li id="dl-list">Ladda ner fil (Schema).</li>
       <li id="edit-list">Redigera schema (i tabell).</li>
@@ -251,6 +255,10 @@ const futureDesc = {
   en: `Handle <u id="current-plan-words">"Future Plans"</u> and watch <u id="past-tasks-words">Past tasks</u>:
     <ul>
       <li id="add-list">Add Task / Upload File (Schedule).</li>
+        <ul>
+          <li>Times are sorted (Format: hh:mm)</li>
+          <li>Recurring tasks can be added (day of week, weeks, month, year + number of days duration)</li>
+        </ul>
       <li id="save-list">Save file (Schedule).</li>
       <li id="dl-list">Download file (Schedule).</li>
       <li id="edit-list">Edit schedule (in table).</li>
