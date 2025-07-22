1b7389452efbb5e40518f090f8b39393be6670ca
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-07-22 09:09:11 +0200

    Update language.js FIX header Todos (top3)

--- scripts/language.js
+++ scripts/language.js
@@ -122,8 +122,8 @@ function getTodaysDayAbbreviation(todaysWeekdayAbbNr) {
 // Översättning Top3 Todos
 
 const top3Todos = {
-  sv: `Top3 Todos`,
-  en: `Top3 Todos`,
+  sv: `Todos (Top3)`,
+  en: `Todos (Top3)`,
 }
 
 const placeholderTop3Text = {
