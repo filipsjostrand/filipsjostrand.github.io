704fdd44850ce1764d6b1ca765a01d641c1f8990
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-04 14:24:39 +0200

    Update navbar.js TEST (navbar scroll rev15)

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,27 +1,54 @@
 let prevScrollPos = window.pageYOffset;
 const navbar = document.getElementById("navbar");
+const todosNav = document.getElementById("top-todos");
+const weekNav = document.getElementById("weekly-calendar");
+const planNav = document.getElementById("important-dates");
+const timerNav = document.getElementById("timer");
+const aboutNav = document.getElementById("about-me");
 
 let isScrolling;
+let isNavbarRolledUp = false;
 
 window.addEventListener('scroll', function () {
   const currentScrollPos = window.pageYOffset;
 
   // Scrolling — hide navbar
-  navbar.style.top = "-50px"; // adjust based on navbar height
+  navbar.style.top = "-60px"; // adjust based on navbar height
   
   window.clearTimeout(isScrolling);
 
-  isScrolling = setTimeout(function () {
+  //isScrolling = setTimeout(function () {
     // Scrolling has stopped!
     // Call showNavbar function:
-    showNavbar(); 
-  }, 10); // adjust delay as needed (300ms is typical)
+    //showNavbar(); 
+  //}, 10); // adjust delay as needed (300ms is typical)
+
+  navbar.addEventListener('click', function() {
+    showNavbar();
+  }
+
+  todosNav.addEventListener('click', function() {
+    showNavbar();
+  }
+   weekNav.addEventListener('click', function() {
+    showNavbar();
+  }
+  planNav.addEventListener('click', function() {
+    showNavbar();
+  }
+  timerNav.addEventListener('click', function() {
+    showNavbar();
+  }
+   aboutNav.addEventListener('click', function() {
+    showNavbar();
+  }
+  
   prevScrollPos = currentScrollPos;
 });
 
 function showNavbar() {
   console.log("showNavbar starts")
-  const navbar = document.getElementById("navbar");
-  
   navbar.style.top = "0"; //fade it in...
 }
+
+
