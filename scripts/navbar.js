e71047590d0ea6b2e500154494f7ad49b0a29aa7
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-04 15:19:27 +0200

    Update navbar.js FIX (navbar scroll show navbar settimeout)

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -10,11 +10,9 @@ let isScrolling;
 let isNavbarRolledUp = false;
 
   navbar.addEventListener('click', function() {
-    window.removeEventListener("scroll", scrollHandler);
-    navbar.style.top = "0";
     console.log("show navbar");
     setTimeout(() => {
-      window.addEventListener("scroll", scrollHandler);
+      navbar.style.top = "0";
       console.log("Scroll listener re-enabled");
     }, 10); // change this delay to suit your needs
   });
