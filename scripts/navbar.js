81709d1aa9a0dac7d311acec2586aff9cdd28ce0
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-04 20:42:05 +0200

    Update navbar.js TEST (navbar scroll rev43)

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -51,6 +51,8 @@ function showNavbar(isNavbarRolledUp, isNavbarClicked, isScrolling, lastScroll) 
   //isNavbarRolledUp = true;
   //isScrolling = true;
   //} 
+
+  console.log("isNavbarRolledUp, isNavbarClicked, isScrolling, lastScroll" = + isNavbarRolledUp + ", " + isNavbarClicked + ", " + isScrolling + ", " + lastScroll)
 }
   //else if (isNavbarRolledUp === false) {
     //navbar.style.top = "0";
