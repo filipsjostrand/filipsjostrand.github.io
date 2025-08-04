fbee9ce36f67765f6557a75035b8a1ff6db6d33a
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-04 13:41:08 +0200

    Update navbar.js TEST (navbar scrolling no fade)

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,14 +2,12 @@ let prevScrollPos = window.pageYOffset;
 const navbar = document.getElementById("navbar");
 
 let isScrolling;
-let opacity = 1;
 
 window.addEventListener('scroll', function () {
-  opacity = 0;
   const currentScrollPos = window.pageYOffset;
 
-      // Scrolling — hide navbar
-      navbar.style.top = "-100px"; // adjust based on navbar height
+  // Scrolling — hide navbar
+  navbar.style.top = "-100px"; // adjust based on navbar height
   
   window.clearTimeout(isScrolling);
 
@@ -24,17 +22,6 @@ window.addEventListener('scroll', function () {
 function showNavbar() {
   console.log("showNavbar starts")
   const navbar = document.getElementById("navbar");
-
-    const fadeIn = setInterval(() => {
-        console.log("fadeIn starts")
-    navbar.style.top = "0"; //fade it in...
-    if (opacity < 1) {
-      opacity += 0.05; // adjust step size for speed
-      navbar.style.opacity = opacity;
-      console.log("navbar.style.opacity = " + navbar.style.opacity)
-    } else {
-      clearInterval(fadeIn); // stop the interval when done
-      console.log("fadeIn done")
-    }
-  }, 50); // adjust timing for smoothness (lower = smoother)
+  
+  navbar.style.top = "0"; //fade it in...
 }
