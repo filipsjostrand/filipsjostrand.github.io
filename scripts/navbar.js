fe372fe8b36ea1cb475da5ac8004cebe34526309
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-04 23:41:08 +0200

    Update navbar.js TEST (navbar scroll rev56)

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,22 +2,23 @@ const navbar = document.getElementById("navbar");
 
 let prevScrollPos = window.pageYOffset;
 
-  window.addEventListener("scroll", function () {
-    const currentScrollPos = window.pageYOffset;
-
-    if (prevScrollPos > currentScrollPos) {
-      // Scrolling up — show navbar
-      navbar.style.top = "0";
-    } else if (prevScrollPos < currentScrollPos) {
-      // Scrolling down — hide navbar
-      navbar.style.top = "-59px"; // adjust based on navbar height
-    }
-           prevScrollPos = currentScrollPos                                   
-  });
-
 // 🎯 Show navbar when any anchor link is clicked
 document.querySelectorAll('a[href^="#"]').forEach(link => {
   link.addEventListener("click", function () {
     navbar.style.top = "0";
   });
-})
+});
+
+window.addEventListener("scroll", function () {
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    // Scrolling up — show navbar
+    navbar.style.top = "0";
+  } else if (prevScrollPos < currentScrollPos) {
+    // Scrolling down — hide navbar
+    navbar.style.top = "-59px"; // adjust based on navbar height
+  }
+
+  prevScrollPos = currentScrollPos;
+});
