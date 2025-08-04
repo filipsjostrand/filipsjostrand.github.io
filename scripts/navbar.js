d2e71306024f8344a27f5a5f1493ae0247abbb25
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-04 11:52:22 +0200

    Create navbar.js ADD (navbar hide when scroll rev01)

---
+++ scripts/navbar.js
@@ -0,0 +1,16 @@
+  let prevScrollPos = window.pageYOffset;
+  const navbar = document.getElementById("navbar");
+
+  window.addEventListener("scroll", function () {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+      // Scrolling up — show navbar
+      navbar.style.top = "0";
+    } else {
+      // Scrolling down — hide navbar
+      navbar.style.top = "-100px"; // adjust based on navbar height
+    }
+
+    prevScrollPos = currentScrollPos;
+  });
