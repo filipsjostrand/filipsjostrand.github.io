edb558efeae1fd007584d410fba256268cf213a8
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-05 04:18:34 +0200

    Update navbar.js  TEST (navbar scroll rev78)

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -15,27 +15,32 @@ window.addEventListener("scroll", function () {
 
 todosNav.addEventListener('click', function () {
   prevScrollPos = 3400;
-  navClicked = true;
+  navClicked = !navClicked;
+  //navClicked = true;
   console.log("A")
 })
 weekNav.addEventListener('click', function () {
   prevScrollPos = 3400;
-  navClicked = true;
+  navClicked = !navClicked;
+  //navClicked = true;
   console.log("B")
 })
 planNav.addEventListener('click', function () {
   prevScrollPos = 3400;
-  navClicked = true;
+  navClicked = !navClicked;
+  //navClicked = true;
   console.log("C")
 })
 timerNav.addEventListener('click', function () {
   prevScrollPos = 3400;
-  navClicked = true;
+  navClicked = !navClicked;
+  //navClicked = true;
   console.log("D")
 })
 aboutNav.addEventListener('click', function () {
   prevScrollPos = 3400;
-  navClicked = true;
+  navClicked = !navClicked;
+  //navClicked = true;
   console.log("E")
 })
   
