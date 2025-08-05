aa475f611cd92e379c5269528e9ca9786dcc60e6
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-05 03:11:30 +0200

    Update navbar.js TEST (navbar scroll rev71)

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -13,26 +13,31 @@ window.addEventListener("scroll", function () {
 
 todosNav.addEventListener('click', function () {
   prevScrollPos = 3400;
+  console.log("prevScrollPos > currentScrollPos (nav) = " + prevScrollPos > currentScrollPos)
 })
 weekNav.addEventListener('click', function () {
   prevScrollPos = 3400;
+  console.log("prevScrollPos > currentScrollPos (nav) = " + prevScrollPos > currentScrollPos)
 })
 planNav.addEventListener('click', function () {
   prevScrollPos = 3400;
+  console.log("prevScrollPos > currentScrollPos (nav) = " + prevScrollPos > currentScrollPos)
 })
 timerNav.addEventListener('click', function () {
   prevScrollPos = 3400;
+  console.log("prevScrollPos > currentScrollPos (nav) = " + prevScrollPos > currentScrollPos)
 })
 aboutNav.addEventListener('click', function () {
   prevScrollPos = 3400;
+  console.log("prevScrollPos > currentScrollPos (nav) = " + prevScrollPos > currentScrollPos)
 })
   
 
-
   if (prevScrollPos > currentScrollPos) {
+    console.log("prevScrollPos > currentScrollPos (i scroll) = " + prevScrollPos > currentScrollPos)
     navbar.style.top = "0"; //scrolling up - show scrollbar
     console.log("scroll up - std ... show navbar (1)")
-  }  
+  }
   else {
     console.log("scroll down - std ... hide navbar (2)")
     navbar.style.top = "-59px"; // - hide scrollbar
