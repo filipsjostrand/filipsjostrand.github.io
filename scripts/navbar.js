db13a38af211e0cc7ce35ba145c8995c2c3b378b
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-05 06:10:42 +0200

    Update navbar.js TEST (scrollAboutTop show navbar)

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,9 +1,20 @@
 const navbar = document.getElementById("navbar");
 
+const aboutDiv = document.getElementById("about");
+let scrollAboutTop = false;
+
 let prevScrollPos = window.pageYOffset;
 
 window.addEventListener("scroll", function () {
 
+  scrollAboutTop = aboutDiv.scrollTop === 0;
+  
+  if (scrollAboutTop) {
+    console.log("You've reached the top of the #about section!");
+    // Add your logic for top-scroll behavior here
+    navbar.style.top = "0";
+  }
+  
   const currentScrollPos = window.pageYOffset;
   
   if (prevScrollPos > currentScrollPos) {
