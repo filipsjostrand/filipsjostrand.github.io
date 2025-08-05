2d991be0a8c6a55b4aa030f97f14d0c4e232060b
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-05 05:15:37 +0200

    Update navbar.js TEST (navbar scroll typo r17)

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -15,13 +15,13 @@ document.addEventListener("DOMContentLoaded", function () {
       isNavActive = !isNavActive;
       if (isNavActive === true) {
         hideText.addEventListener("click", function () {
-          if (hideText.innerHTML === "❌ Navbar") {
+          if (hideText.innerText === "❌ Navbar") {
             navbar.style.top = "-59px"; // hides navbar
             hideText.style.fontSize = "16px";
             //hideText.style.display = "none"; // hides the text
         } else if (isNavActive === false) {
             navbar.style.top = "0"; // hides navbar
-            hideText.innerHTML = "☰";
+            hideText.innerText = "☰";
             hideText.style.fontSize = "20px";
           }
         });
