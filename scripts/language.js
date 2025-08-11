09a7022d95758beb475400d3648af3e112057b6a
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-08-11 20:14:07 +0200

    Update language.js ADD (appInfoWeekNr)

--- scripts/language.js
+++ scripts/language.js
@@ -151,6 +151,11 @@ const appInfoWeek = {
   en: `Week`,
 }
 
+const appInfoWeekNr = {
+  sv: `Vecka: `,
+  en: `Week: `,
+}
+
 const appInfoFuture = {
   sv: `Plan`,
   en: `Future`,
@@ -858,7 +863,7 @@ const userMessagePlaceholder = {
           document.querySelector('.info').innerHTML = infoBullets[selectedLanguage];
           
           document.getElementById('weekday-display').innerHTML = getTodaysDayAbbreviation(todaysWeekdayAbbNr);
-          document.getElementById('week-display').innerHTML = appInfoWeek[selectedLanguage];
+          document.getElementById('week-display').innerHTML = appInfoWeekNr[selectedLanguage];
 
           document.querySelector('.add-button-v2').innerHTML = addButton2[selectedLanguage];
           document.getElementById('top3-label').innerHTML = top3Label[selectedLanguage];
@@ -1058,7 +1063,3 @@ const userMessagePlaceholder = {
         }
 
 runTranslation(selectedLanguage);
-
-
-
-
