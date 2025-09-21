38f852fe2aa135360bb2047ef660f1196a0d6f66
Filip Sjöstrand <92868216+filipsjostrand@users.noreply.github.com>
2025-09-21 18:07:07 +0200

    Update daily-quotes.js FIX (mousedown)

--- scripts/daily-quotes.js
+++ scripts/daily-quotes.js
@@ -8333,7 +8333,7 @@ dailyBibleQuoteContainer.addEventListener("click", function (e) {
 
 
   // Get Bible quote when clicking on pidgeon icon
-  bibleQuoteContainerIcon.addEventListener("onmousedown", function () {
+  bibleQuoteContainerIcon.addEventListener("mousedown", function () {
     if (dailyBibleQuoteContainer.style.opacity === "0" || dailyBibleQuoteContainer.style.opacity === "") {
       dailyBibleQuoteContainer.style.display = "flex";
       dailyBibleQuoteParagraph.style.opacity = "0";
